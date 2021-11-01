4d5ded7b60e3ead371ac197c45fa927af034252d
Nikos Boumakis <csd4346@cd.uoc.gr>
2021-11-01 15:35:26 +0200

    Define new_parser

--- parser.c
+++ parser.c
@@ -5,3 +5,15 @@ struct sh_parser {
     int cur_token_index; /* Hold the index of the token last returned */
     int token_count;     /* Hold the total number of tokens */
 };
+
+sh_parser_t new_parser() {
+    sh_parser_t parser_new = malloc(sizeof(struct sh_parser));
+
+    if (parser_new != NULL) {
+        parser_new->cur_token_index = 0;
+        parser_new->token_count = 0;
+        parser_new->tokens = NULL;
+    }
+
+    return parser_new;
+}
