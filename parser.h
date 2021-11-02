4cfe3ca7caaf888e260afeed4c884393c5568f8e
Nikos Boumakis <csd4346@cd.uoc.gr>
2021-11-03 00:14:45 +0200

    Contract update: Add missing functions previous_token & current_token

--- parser.h
+++ parser.h
@@ -17,5 +17,14 @@ void clear_parser(sh_parser_t);
 /* Clears and deallocates the parser */
 void delete_parser(sh_parser_t);
 
+/* Return the next token and move current to be the token forward.
+ * Returns NULL if out of bounds. */
 char *next_token(sh_parser_t);
+
+/* Return the previous token and move current to be the token backwards.
+ * Returns NULL if out of bounds */
+char *previous_token(sh_parser_t);
+
+/* Return the current token or NULL if out of bounds */
+char *current_token(sh_parser_t);
 #endif
