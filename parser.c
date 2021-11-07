33a5f38d6d7b8b5c742b52c64ddf700c92f614b4
Nikos Boumakis <csd4346@cd.uoc.gr>
2021-11-08 01:30:26 +0200

    Move parser to parser

--- parser.c
+++ parser.c
@@ -1,198 +1,216 @@
-#include "parser.h"
+#include "parser1.h"
+#include "parserUtilities.h"
 #include <ctype.h>
 #include <string.h>
 
-struct sh_parser {
-    char **tokens;       /* Hold each token as a string */
-    int cur_token_index; /* Hold the index of the token last returned */
-    int token_count;     /* Hold the total number of tokens */
+/* A struct to create a doubly linked list, with each cell containing one simple
+ * command */
+struct command_list_cell {
+    command_simple_t command;       /* A pointer to the simple command */
+    struct command_list_cell *next; /* Pointer to the next cell */
+    struct command_list_cell *prev; /* Poointer to the previous cell */
 };
 
-enum parser_states { TEXT, MULTIPLE, PIPE, REDIR_IN, REDIR_OUT, WHITESPACE };
+/* A struct containing the command sequence elements and ways to move among them */
+struct command_sequence {
+    struct command_list_cell
+        *command_list_head; /* A pointer to the head of the simple command list */
+    struct command_list_cell
+        *command_list_tail; /* A pointer to the tail of the simple command list */
+    struct command_list_cell
+        *current_command; /* A pointer to the current simple command */
+    int command_count;    /* The total number of simple commands */
+};
 
-sh_parser_t new_parser() {
-    sh_parser_t parser_new = malloc(sizeof(struct sh_parser));
+struct command_simple {
+    char **command_and_args; /* A array of strings containing the command and its
+                                arguments */
+    int command_args_count;
 
-    if (parser_new != NULL) {
-        parser_new->cur_token_index = 0;
-        parser_new->token_count = 0;
-        parser_new->tokens = NULL;
-    }
+    int input_redirection; /* Boolean, whether redirection is used */
+    char *input_filename;  /* The filename for the input */
 
-    return parser_new;
-}
+    int output_redirection;       /* Boolean, whether redirection is used */
+    char *output_filename;        /* The filename for the output */
+    enum OUTPUT_TYPE output_type; /* The type of the output */
 
-static int is_separator(char input) {
-    if (input == '|' || input == '<' || input == '>' || input == ';') {
-        return 1;
-    }
+    int pipeIn;  /* Boolean, whether to use input from the previous pipe */
+    int pipeOut; /* Boolean, whether to write the output to the next pipe */
+};
 
-    return 0;
-}
+static void insert_after_simple_command_list(struct command_list_cell **head,
+                                             struct command_list_cell **tail,
+                                             command_simple_t simple_command) {
+    struct command_list_cell *new_cell = malloc(sizeof(struct command_list_cell));
 
-enum parser_states next_state(char c) {
-    if (c == ';') {
-        return MULTIPLE;
-    } else if (isspace(c)) {
-        return WHITESPACE;
-    } else if (c == '|') {
-        return PIPE;
-    } else if (c == '<') {
-        return REDIR_IN;
-    } else if (c == '>') {
-        return REDIR_OUT;
-    } else {
-        return TEXT;
-    }
-}
+    new_cell->command = simple_command;
 
-enum parser_states action_text(char c, int *count) {
-    enum parser_states new_state = next_state(c);
+    if (*head == NULL) {
+        new_cell->next = NULL;
+        new_cell->prev = NULL;
 
-    if (new_state != TEXT || c == '\0')
-        (*count)++;
-
-    return new_state;
-}
-enum parser_states action_multiple(char c, int *count) {
-    (*count)++;
-
-    return next_state(c);
-}
-enum parser_states action_pipe(char c, int *count) {
-    (*count)++;
-
-    return next_state(c);
-}
-enum parser_states action_redir_in(char c, int *count) {
-    (*count)++;
-
-    return next_state(c);
-}
-enum parser_states action_redir_out(char c, int *count) {
-    if (c == '>')
-        return REDIR_OUT;
-    else {
-        (*count)++;
-        return next_state(c);
-    }
-}
-
-enum parser_states action_whitespace(char c, int *count) { return next_state(c); }
-
-static int count_tokens(char *input, size_t input_length) {
-    int count = 0;
-
-    enum parser_states (*actions[6])(char, int *) = {
-        action_text,     action_multiple,  action_pipe,
-        action_redir_in, action_redir_out, action_whitespace};
-    enum parser_states state;
-
-    int i = 0;
-
-    /* Only \0 is present in the input */
-    if (input_length == 1) {
-        return 0;
-    }
-
-    state = next_state(input[0]);
-
-    while (i < input_length) {
-        state = actions[state](input[i], &count);
-        i++;
-    }
-
-    return count;
-}
-
-void input_parse(sh_parser_t parser, char *input, size_t input_length) {
-    int count = 0;
-    char **token_arr;
-    char *input_tmp = input;
-    size_t i = 0, token_size, token_start, token_end;
-
-    count = count_tokens(input, input_length);
-    parser->token_count = count;
-    parser->cur_token_index = 0;
-
-    if (count == 0) {
-        parser->tokens = NULL;
+        *head = new_cell;
+        *tail = new_cell;
 
         return;
     }
 
-    token_arr = malloc(sizeof(char *) * count);
+    new_cell->next = (*tail)->next;
+    new_cell->prev = *tail;
+    (*tail)->next = new_cell;
+    *tail = new_cell;
 
-    token_start = 0;
-    for (i = 0; i < count; i++) {
-        while (isspace(input[token_start]))
-            token_start++;
+    return;
+}
 
-        token_end = token_start;
+char **get_command_and_arguments(command_simple_t simple_command) {
+    return simple_command->command_and_args;
+}
 
-        if (!is_separator(
-                input[token_start])) { /* The current token is not a separator, look
-                                          until the first or a space */
-            while (token_end < input_length && !is_separator(input[token_end]) &&
-                   !isspace(input[token_end]))
-                token_end++;
+char *get_input_filename(command_simple_t simple_command) {
+    if (simple_command->input_redirection)
+        return simple_command->input_filename;
+    else
+        return NULL;
+}
 
-            /* Move the end back by one so token_end points to the last character of
-             * the token*/
-            token_end--;
+char *get_output_filename(command_simple_t simple_command) {
+    if (simple_command->output_redirection)
+        return simple_command->output_filename;
+    else
+        return NULL;
+}
+
+enum OUTPUT_TYPE get_output_type(command_simple_t simple_command) {
+    return simple_command->output_type;
+}
+
+int get_pipe_input(command_simple_t simple_command) {
+    return simple_command->pipeIn;
+}
+
+int get_pipe_output(command_simple_t simple_command) {
+    return simple_command->pipeOut;
+}
+
+command_simple_t get_next_simple_command(command_sequence_t command_seq) {
+    command_seq->current_command = command_seq->current_command->next;
+
+    return command_seq->current_command->command;
+}
+
+command_simple_t get_previous_simple_command(command_sequence_t command_seq) {
+    command_seq->current_command = command_seq->current_command->prev;
+
+    return command_seq->current_command->command;
+}
+
+command_simple_t get_current_simple_command(command_sequence_t command_seq) {
+    return command_seq->current_command->command;
+}
+
+/* Parses the read command or series of commands */
+command_sequence_t parse(char *input, size_t input_size,
+                         command_sequence_t command_seq) {
+    int i, input_index = 0, arg_count = 0;
+    command_simple_t tmp_simple;
+    char **cmd_args;
+    size_t cmd_start_index, cmd_end_index, arg_end_index;
+    int prevPipeOut = 0;
+
+    if (command_seq == NULL) {
+        command_seq = malloc(sizeof(struct command_sequence));
+    }
+
+    command_seq->command_list_head = NULL;
+    command_seq->command_list_tail = NULL;
+    command_seq->command_count = count_simple_commands(input, input_size);
+
+    for (i = 0; i < command_seq->command_count; i++) {
+        tmp_simple = malloc(sizeof(struct command_simple));
+        tmp_simple->input_redirection = 0;
+        tmp_simple->input_filename = NULL;
+        tmp_simple->output_redirection = 0;
+        tmp_simple->output_filename = NULL;
+
+        cmd_start_index = find_command_beginning(input, cmd_start_index, input_size);
+        cmd_end_index = find_command_end(input, cmd_start_index, input_size);
+        arg_end_index = find_arg_end(input, cmd_start_index, cmd_end_index);
+
+        tmp_simple->command_args_count =
+            count_arguments(input, cmd_start_index, arg_end_index);
+
+        tmp_simple->command_and_args = extract_cmd_args(
+            tmp_simple->command_args_count, input, cmd_start_index, arg_end_index);
+
+        if (check_input_redirection(input, arg_end_index + 1, cmd_end_index)) {
+            tmp_simple->input_redirection = 1;
+            tmp_simple->input_filename = get_filename_input_redirection(
+                input, arg_end_index + 1, cmd_end_index);
+        }
+
+        if (check_output_redirection(input, arg_end_index + 1, cmd_end_index)) {
+            tmp_simple->output_redirection = 1;
+            tmp_simple->output_type =
+                get_output_redir_type(input, arg_end_index + 1, cmd_end_index);
+            tmp_simple->output_filename = get_filename_output_redirection(
+                input, arg_end_index + 1, cmd_end_index);
+        }
+
+        if (check_pipeOut(input, cmd_start_index, cmd_end_index)) {
+            if (!tmp_simple->output_redirection) {
+                tmp_simple->pipeOut = 1;
+                prevPipeOut = 1;
+            } else {
+                prevPipeOut = 0;
+            }
         } else {
-            if (token_start + 1 < input_length && input[token_start] == '>' &&
-                input[token_start + 1] == '>') {
-                token_end++;
+            prevPipeOut = 0;
+        }
+
+        if (prevPipeOut) {
+            if (!tmp_simple->input_redirection) {
+                tmp_simple->pipeIn = 1;
             }
         }
 
-        token_size = token_end - token_start + 1;
-        token_arr[i] = malloc((token_size + 1) * sizeof(char));
-
-        strncpy(token_arr[i], &input[token_start], token_size);
-        token_arr[i][token_size] = '\0';
-
-        token_start = token_end + 1;
+        insert_after_simple_command_list(&(command_seq->command_list_head),
+                                         &(command_seq->command_list_tail),
+                                         tmp_simple);
     }
-    parser->tokens = token_arr;
+
+    command_seq->current_command = command_seq->command_list_head;
+
+    return command_seq;
 }
 
-void clear_parser(sh_parser_t parser) {
+void clear_command_sequence(command_sequence_t command_seq) {
+    struct command_list_cell *tmp = command_seq->command_list_head, *next;
     int i;
 
-    for (i = 0; i < parser->token_count; i++) {
-        free(parser->tokens[i]);
-        parser->tokens[i] = NULL;
+    while (tmp != NULL) {
+        next = tmp->next;
+
+        for (i = 0; i < tmp->command->command_args_count; i++) {
+            free(tmp->command->command_and_args[i]);
+        }
+        free(tmp->command->command_and_args);
+
+        free(tmp->command->input_filename);
+        free(tmp->command->output_filename);
+
+        free(tmp->command);
+        free(tmp);
+
+        tmp = next;
     }
 
-    free(parser->tokens);
-    parser->tokens = NULL;
-
-    parser->token_count = 0;
-    parser->cur_token_index = 0;
+    command_seq->current_command = NULL;
+    command_seq->command_list_head = NULL;
+    command_seq->command_count = 0;
 }
 
-void delete_parser(sh_parser_t parser) {
-    clear_parser(parser);
-    free(parser);
-}
-
-char *next_token(sh_parser_t parser) {
-    parser->cur_token_index++;
-    return current_token(parser);
-}
-
-char *previous_token(sh_parser_t parser) {
-    parser->cur_token_index--;
-    return current_token(parser);
-}
-
-char *current_token(sh_parser_t parser) {
-    if (parser->cur_token_index < 0 ||
-        parser->cur_token_index >= parser->token_count) {
-        return NULL;
-    }
-
-    return parser->tokens[parser->cur_token_index];
+void free_command_sequence(command_sequence_t command_seq) {
+    clear_command_sequence(command_seq);
+    free(command_seq);
 }
--- parser.h
+++ parser.h
@@ -3,28 +3,36 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-typedef struct sh_parser *sh_parser_t;
+typedef struct command_sequence *command_sequence_t;
+typedef struct command_simple *command_simple_t;
 
-/* Creates and returns a new parser */
-sh_parser_t new_parser();
+enum OUTPUT_TYPE { OVERWRITE, APPEND };
 
 /* Parses the read command or series of commands */
-void input_parse(sh_parser_t, char *, size_t);
+command_sequence_t parse(char *, size_t, command_sequence_t);
 
 /* Clears the parser and its fields allowing it to be reused */
-void clear_parser(sh_parser_t);
+void clear_command_sequence(command_sequence_t);
 
 /* Clears and deallocates the parser */
-void delete_parser(sh_parser_t);
+void free_command_sequence(command_sequence_t);
 
 /* Return the next token and move current to be the token forward.
  * Returns NULL if out of bounds. */
-char *next_token(sh_parser_t);
+command_simple_t get_next_simple_command(command_sequence_t);
 
 /* Return the previous token and move current to be the token backwards.
  * Returns NULL if out of bounds */
-char *previous_token(sh_parser_t);
+command_simple_t get_previous_simple_command(command_sequence_t);
 
 /* Return the current token or NULL if out of bounds */
-char *current_token(sh_parser_t);
+command_simple_t get_current_simple_command(command_sequence_t);
+
+char **get_command_and_arguments(command_simple_t);
+char *get_input_filename(command_simple_t);
+char *get_output_filename(command_simple_t);
+enum OUTPUT_TYPE get_output_type(command_simple_t);
+int get_pipe_input(command_simple_t);
+int get_pipe_output(command_simple_t);
+
 #endif
